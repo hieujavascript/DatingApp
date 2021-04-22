0d1cd793d7b4f17c5d5bd9002ea71dc497fe9dec
hieujavascript <hieujavascript@gmail.com>
2021-04-22 11:03:26 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ namespace API
 {
     public class Startup
     {
+     
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
@@ -31,7 +32,9 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            // add connec string
+        // =========== add Cors =================
+        services.AddCors();
+            //=========== add connec string
             services.AddDbContext<DataContext>( options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             } );
@@ -41,6 +44,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +60,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
